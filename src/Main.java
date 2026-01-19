a0e8322833b1c044a3670e4dc856cb1bfbf2de7a
donal <25458345@studentmail.ul.ie>
2026-01-19 14:45:09 +0000

    Creation of main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//All static classes, methods and objects will be called here
+//This is where the game is played
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+}
