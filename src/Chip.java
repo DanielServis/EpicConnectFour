a53ae60fdac487d07d7979b6a5d3c15fd7accd91
donal <25458345@studentmail.ul.ie>
2026-01-19 15:16:25 +0000

    Red chip sprite

--- src/Chip.java
+++ src/Chip.java
@@ -1,8 +1,41 @@
 public class Chip {
+
     boolean isRed;
+
+    String[] iomha = new String[5];
 
     Chip(boolean isRed)
     {
         this.isRed = isRed;
+
+        if (isRed){
+            iomha[0] = "   _c'000'c_   ";
+            iomha[1] = " /00000000000\\ ";
+            iomha[2] = "|0000000000000|";
+            iomha[3] = "|0000000000000|";
+            iomha[4] = " \\0000000000/  ";
+            iomha[5] = "  '*=Y000P=*'  ";
+        }
+        else
+        {
+            iomha[0] = "";
+            iomha[1] = "";
+            iomha[2] = "";
+            iomha[3] = "";
+            iomha[4] = "";
+        }
+    }
+
+    public boolean getRed()
+    {
+        if (isRed)
+            return true;
+        else
+            return false;
+    }
+
+    public String[] printChip()
+    {
+        return iomha;
     }
 }
