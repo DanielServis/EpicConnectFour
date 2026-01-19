7f940602c81b811f6569607d03d6865069d23e68
donal <25458345@studentmail.ul.ie>
2026-01-19 15:27:15 +0000

    Red and Yellow chip sprites & getRed() method

--- src/Chip.java
+++ src/Chip.java
@@ -2,27 +2,28 @@ public class Chip {
 
     boolean isRed;
 
-    String[] iomha = new String[5];
+    String[] iomha = new String[6];
 
     Chip(boolean isRed)
     {
         this.isRed = isRed;
 
         if (isRed){
-            iomha[0] = "   _c'000'c_   ";
-            iomha[1] = " /00000000000\\ ";
-            iomha[2] = "|0000000000000|";
-            iomha[3] = "|0000000000000|";
-            iomha[4] = " \\0000000000/  ";
-            iomha[5] = "  '*=Y000P=*'  ";
+            iomha[0] = "\u001B[31m   _c'000'c_   ";
+            iomha[1] = "\u001B[31m /00000000000\\ ";
+            iomha[2] = "\u001B[31m|0000000000000|";
+            iomha[3] = "\u001B[31m|0000000000000|";
+            iomha[4] = "\u001B[31m \\00000000000/ ";
+            iomha[5] = "\u001B[31m  '*=Y000P=*'  ";
         }
         else
         {
-            iomha[0] = "";
-            iomha[1] = "";
-            iomha[2] = "";
-            iomha[3] = "";
-            iomha[4] = "";
+            iomha[0] = "\u001B[33m   _c'000'c_   ";
+            iomha[1] = "\u001B[33m /00000000000\\ ";
+            iomha[2] = "\u001B[33m|0000000000000|";
+            iomha[3] = "\u001B[33m|0000000000000|";
+            iomha[4] = "\u001B[33m \\00000000000/ ";
+            iomha[5] = "\u001B[33m  '*=Y000P=*'  ";
         }
     }
 
